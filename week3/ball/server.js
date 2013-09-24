4236d9c582e9234a13d8a314fde922b0f2270d31
mariapaulasaba <msd403@nyu.edu>
2013-09-23 22:59:41 -0400

    last homework update

--- week3/ball/server.js
+++ week3/ball/server.js
@@ -19,33 +19,40 @@ function requestHandler(req, res) {
 			res.end(data);
   		}
   	);
-}
+};
 
 
 // WebSocket Portion
 // WebSockets work with the HTTP server
 var io = require('socket.io').listen(httpServer);
 
+var clients = {};
+var clientId = -1;
+
 // Register a callback function to run when we have an individual connection
 // This is run for each individual user that connects
 io.sockets.on('connection', 
 	// We are given a websocket object in our function
 	function (socket) {
-	
-		console.log("We have a new client: " + socket.id);
-		
-		// When this user emits, client side: socket.emit('otherevent',some data);
-		socket.on('othermouse', function(data) {
-			// Data comes in as whatever was sent, including objects
-			console.log("Received: 'othermouse' " + data.x + " " + data.y);
-			
-			// Send it to all of the clients
-			socket.broadcast.emit('othermouse', data);
+		clientId++;
+		socket.clientId = clientId;
+		clients[clientId] = socket;
+
+		//console.log("We have a new client: " + socket.id);
+		socket.on('createBall', function(data){
+			console.log('createBall: ' + clientId);			
+			socket.broadcast.emit('createBall',clientId);
+			io.sockets.emit('myBall', clientId);
 		});
-		
+
+		socket.on('sendmouse', function(data){
+			socket.broadcast.emit('sendmouse', data);
+		});
 		
 		socket.on('disconnect', function() {
 			console.log("Client has disconnected " + socket.id);
+			socket.broadcast.emit('destroyBall', this.clientId);
+			delete clients[this.clientId];
 		});
 	}
 );
